462a73b14432d4f92e4b8f1729a14c9df0078387
MikeCamacho <michaelsamuelca@gmail.com>
2019-06-26 14:34:33 -0500

    Banner New

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,57 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './Banner.scss';
-import ImgLeft from '../../assets/images/solicitar-asesoria-banner.jpg';
 
-const banner = props => (
-    <div className="banner-home">
-        <div className="banner-home__left">
-          <a href="/">Solicitar Asesoria</a>
-        </div>
-        <div className="banner-home__right">
-          <a href="/">Registrate Gratis</a>
-        </div>
-    </div>
-);
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-export default banner;
+export default class ChangeSlide extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      class: "banner-home",
+      content: "Alcanza metas facilmente sin perder tiempo en infraestructura y sin perder de vista tu obejtivo real",
+    };
+  }
+
+  cambiarClass = ()=>{
+    this.setState({class: "banner-home2", content:"Alcanza metas facilmente sin perder tiempo en infraestructura y sin perder de vista tu obejtivo real2" })
+
+  }
+  cambiarClass2 = ()=>{
+    this.setState({class: "banner-home", content:"Alcanza metas facilmente sin perder tiempo en infraestructura y sin perder de vista tu obejtivo real"})
+  }
+
+  render(){
+    return (
+      <div className={this.state.class} id="banner">
+        <div className="container">
+          <div className="col-lg-5 col-12 banner-home__title">
+            <h1>Crea<span>mos</span></h1>
+            <h1>Desarrolla<span>mos</span></h1>
+            <h1 className="last">Funciones</h1>
+            <p> sin Servidor </p>
+          </div>
+          <div className="col-lg-5 col-12 banner-home__descrip">
+            <p>{this.state.content}</p>
+            <p>Ahorra tiempo y dinero con Poetri</p>
+          </div>
+          <div className="col-lg-5 col-12 banner-home__buttons">
+          <Tabs defaultIndex = {1}>
+            <TabList>
+              <Tab onClick={this.cambiarClass}>Soy desarrollador</Tab>
+              <Tab onClick={this.cambiarClass2}>Soy Empresa</Tab>
+            </TabList>
+            <TabPanel>
+              <a>Probar Ahora</a>
+            </TabPanel>
+            <TabPanel>
+              <a>Solicitar Asesoria</a>
+            </TabPanel>
+          </Tabs>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/HowFunction/dummyData.js
@@ -0,0 +1,27 @@
+/* eslint max-len: 0 */
+
+export const presidents = [
+  {
+    icon: './images/consume-funciones.svg',
+    name: 'George Washington',
+    descrip: 'mike',
+    number: '1',
+    content__item: 'Descarga e instala el CLI para comenzar.',
+    valueIn: 'functions.poetri.co/*function'
+  },
+  {
+    icon: './images/consume-funciones.svg',
+    name: 'nnn',
+    descrip: 'mike',
+    number: '2',
+    content__item: 'Desde la consola de comandos escribe:',
+    valueIn: 'elmikis',
+  },
+  {
+    name: 'Abraham Lincoln',
+    biography: 'Abraham Lincoln (February 12, 1809 – April 15, 1865) was the 16th President of the United States',
+  },
+
+];
+
+export default presidents;
